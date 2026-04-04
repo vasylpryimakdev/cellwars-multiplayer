6960b4cae5e4118c6c62d8c712de0533946b410d
Vasyl Pryimak <vasyl.pryimak.dev@gmail.com>
2026-04-04 18:02:39 +0300

    feat: initialize Express server with Socket.IO for multiplayer game

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
-console.log("Hello world!");
+const express = require("express");
+const socketio = require("socket.io");
+
+const app = express();
+
+app.use(express.static(__dirname + "/public"));
+
+const server = app.listen(5000, () => {
+  console.log("Server is running on port: 5000");
+});
+const io = socketio(server);
