adc5f992a16c9b376fbff3ba994db5e1cf406d7b
Vasyl Pryimak <vasyl.pryimak.dev@gmail.com>
2026-04-04 18:28:47 +0300

    refactor: modularize server architecture for better organization

--- server.js
+++ server.js
@@ -8,4 +8,10 @@ app.use(express.static(__dirname + "/public"));
 const server = app.listen(5000, () => {
   console.log("Server is running on port: 5000");
 });
+
 const io = socketio(server);
+
+module.exports = {
+  app,
+  io,
+};
---
+++ socketStuff/socketMain.js
@@ -0,0 +1,3 @@
+const { io } = require("../server");
+
+module.exports = io;
