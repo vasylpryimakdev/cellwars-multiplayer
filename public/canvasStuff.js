abece951c4a9649ce2ce433f181ea5e23b20b6ab
Vasyl Pryimak <vasyl.pryimak.dev@gmail.com>
2026-04-04 19:53:29 +0300

    feat: implement player movement and real-time canvas rendering

--- public/canvasStuff.js
+++ public/canvasStuff.js
@@ -2,19 +2,61 @@ const init = () => {
   draw();
 };
 
+player.locX = Math.floor(500 * Math.random() + 10);
+player.locY = Math.floor(500 * Math.random() + 10);
+
 //================================
 //=============DRAW===============
 //================================
 
 const draw = () => {
-  let randomX = Math.floor(500 * Math.random() + 10);
-  let randomY = Math.floor(500 * Math.random() + 10);
-
+  context.clearRect(0, 0, canvas.width, canvas.height);
   context.beginPath();
   context.fillStyle = "rgb(255, 0, 0)";
-  context.arc(randomX, randomY, 10, 0, Math.PI * 2);
+  context.arc(player.locX, player.locY, 10, 0, Math.PI * 2);
   context.fill();
   context.lineWidth = 3;
   context.strokeStyle = "rgb(255, 0, 0)";
   context.stroke();
+  requestAnimationFrame(draw);
 };
+
+canvas.addEventListener("mousemove", (event) => {
+  const mousePosition = {
+    x: event.clientX,
+    y: event.clientY,
+  };
+  const angleDeg =
+    (Math.atan2(
+      mousePosition.y - canvas.height / 2,
+      mousePosition.x - canvas.width / 2,
+    ) *
+      180) /
+    Math.PI;
+  if (angleDeg >= 0 && angleDeg < 90) {
+    xVector = 1 - angleDeg / 90;
+    yVector = -(angleDeg / 90);
+  } else if (angleDeg >= 90 && angleDeg <= 180) {
+    xVector = -(angleDeg - 90) / 90;
+    yVector = -(1 - (angleDeg - 90) / 90);
+  } else if (angleDeg >= -180 && angleDeg < -90) {
+    xVector = (angleDeg + 90) / 90;
+    yVector = 1 + (angleDeg + 90) / 90;
+  } else if (angleDeg < 0 && angleDeg >= -90) {
+    xVector = (angleDeg + 90) / 90;
+    yVector = 1 - (angleDeg + 90) / 90;
+  }
+
+  speed = 10;
+  xV = xVector;
+  yV = yVector;
+
+  if ((player.locX < 5 && xV < 0) || (player.locX > 500 && xV > 0)) {
+    player.locY -= speed * yV;
+  } else if ((player.locY < 5 && yV > 0) || (player.locY > 500 && yV < 0)) {
+    player.locX += speed * xV;
+  } else {
+    player.locX += speed * xV;
+    player.locY -= speed * yV;
+  }
+});
