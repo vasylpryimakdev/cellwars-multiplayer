ba7f5339b54654477268f76e96d341603d01502e
Vasyl Pryimak <vasyl.pryimak.dev@gmail.com>
2026-04-04 19:29:26 +0300

    feat: implement basic canvas drawing functionality

--- public/canvasStuff.js
+++ public/canvasStuff.js
@@ -1,3 +1,20 @@
 const init = () => {
-  console.log("init");
+  draw();
+};
+
+//================================
+//=============DRAW===============
+//================================
+
+const draw = () => {
+  let randomX = Math.floor(500 * Math.random() + 10);
+  let randomY = Math.floor(500 * Math.random() + 10);
+
+  context.beginPath();
+  context.fillStyle = "rgb(255, 0, 0)";
+  context.arc(randomX, randomY, 10, 0, Math.PI * 2);
+  context.fill();
+  context.lineWidth = 3;
+  context.strokeStyle = "rgb(255, 0, 0)";
+  context.stroke();
 };
