eae8ce48eb602d3d94457f7b00d1af0ab84a0d76
Vasyl Pryimak <vasyl.pryimak.dev@gmail.com>
2026-04-05 23:29:32 +0300

    feat: create Orb class for game object management

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.static(__dirname + "/public"));
 
 const server = app.listen(5000, () => {
-  console.log("Server is running on port: 5000");
+  console.log("Server is running on port: http://localhost:5000");
 });
 
 const io = socketio(server);
