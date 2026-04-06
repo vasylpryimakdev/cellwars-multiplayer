4b6afc16a1403c41f6f965180478608bb62eb5a9
Vasyl Pryimak <vasyl.pryimak.dev@gmail.com>
2026-04-06 15:56:57 +0300

    feat: implement server-side leaderboard system and player disconnect handling

--- expressStuff/expressMain.js
+++ expressStuff/expressMain.js
@@ -1,3 +1,5 @@
-const { app } = require("../servers");
+//The purpose of expressMain is to be the entrypoint for all Express stuff
+const app = require('../servers').app;
+const io = require('../servers').io;
 
 module.exports = app;
