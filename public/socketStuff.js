6f059cc77427aef7aeddd68a7ef2690342ba3e3c
Vasyl Pryimak <vasyl.pryimak.dev@gmail.com>
2026-04-06 14:32:05 +0300

    feat: implement comprehensive game mechanics and collision detection

--- public/socketStuff.js
+++ public/socketStuff.js
@@ -1,10 +1,19 @@
 const socket = io.connect("http://localhost:5000");
 
 const init = async () => {
-  //init is called inside of start-game click listener
   const initData = await socket.emitWithAck("init", {
     playerName: player.name,
   });
+
+  setInterval(async () => {
+    socket.emit("tock", {
+      xVector: player.xVector ? player.xVector : 0.1,
+      yVector: player.yVector ? player.yVector : 0.1,
+    });
+  }, 33);
+
+  orbs = initData.orbs;
+  player.indexInPlayers = initData.indexInPlayers;
 
   orbs = initData;
 
--- socketStuff/socketMain.js
+++ socketStuff/socketMain.js
@@ -23,6 +23,8 @@ let tickTockInterval;
 initGame();
 
 io.on("connection", (socket) => {
+  let player = {};
+
   socket.on("init", (playerObj, ackCallback) => {
     if (players.length === 0) {
       tickTockInterval = setInterval(() => {
@@ -34,10 +36,68 @@ io.on("connection", (socket) => {
     const playerName = playerObj.playerName;
     const playerConfig = new PlayerConfig(settings);
     const playerData = new PlayerData(playerName, settings);
-    const player = new Player(socket.id, playerConfig, playerData);
+    player = new Player(socket.id, playerConfig, playerData);
     players.push(player);
 
     ackCallback(orbs);
+  });
+
+  socket.on("tock", (data) => {
+    //a tock has come in before the player is set up.
+    //this is because the client kept tocking after disconnect
+    if (!player.playerConfig) {
+      return;
+    }
+    speed = player.playerConfig.speed;
+    const xV = (player.playerConfig.xVector = data.xVector);
+    const yV = (player.playerConfig.yVector = data.yVector);
+
+    //if player can move in the x, move
+    if (
+      (player.playerData.locX > 5 && xV < 0) ||
+      (player.playerData.locX < settings.worldWidth && xV > 0)
+    ) {
+      player.playerData.locX += speed * xV;
+    }
+    //if player can move in the y, move
+    if (
+      (player.playerData.locY > 5 && yV > 0) ||
+      (player.playerData.locY < settings.worldHeight && yV < 0)
+    ) {
+      player.playerData.locY -= speed * yV;
+    }
+
+    //check for the tocking player to hit orbs
+    const capturedOrbI = checkForOrbCollisions(
+      player.playerData,
+      player.playerConfig,
+      orbs,
+      settings,
+    );
+
+    if (capturedOrbI !== null) {
+      orbs.splice(capturedOrbI, 1, new Orb(settings));
+
+      const orbData = {
+        capturedOrbI,
+        newOrb: orbs[capturedOrbI],
+      };
+
+      io.to("game").emit("orbSwitch", orbData);
+      io.to("game").emit("updateLeaderBoard", getLeaderBoard());
+    }
+
+    const absorbData = checkForPlayerCollisions(
+      player.playerData,
+      player.playerConfig,
+      players,
+      playersForUsers,
+      socket.id,
+    );
+    if (absorbData) {
+      io.to("game").emit("playerAbsorbed", absorbData);
+      io.to("game").emit("updateLeaderBoard", getLeaderBoard());
+    }
   });
 
   socket.on("disconnect", (reason) => {
